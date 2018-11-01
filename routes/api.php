523ab803f836e66fad21a63e6ae52caf1bed8d83
hehe <1605882811@qq.com>
2018-11-01 09:24:46 +0800

    user can register using phone

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,10 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ],function ($api){
+    //短信验证
     $api->post('verificationCodes','VerificationCodesController@store')
         ->name('api.verificationCodes.store');
+    //用户注册
+    $api->post('users', 'UsersController@store')
+        ->name('api.users,store');
 });
